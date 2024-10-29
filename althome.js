dcabc848ac4b2d4f13ef0ca7ab9c3a4e23ce2bcf
Abdul Hameed Sabree <37881368+ahs4448@users.noreply.github.com>
2024-10-29 17:41:34 -0400

    Update althome.js

--- althome.js
+++ althome.js
@@ -6,6 +6,41 @@
 //     // Here you can add functionality to handle the subscription
 // });
 
+
+
+
+//modal
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -48,6 +83,7 @@ const menuIcon = document.getElementById('menu-icon');
 const navLinks = document.getElementById('nav-links');
 const closeIcon = document.getElementById('close-icon');
 
+
 menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('active'); // Toggle the active class
 });
@@ -55,3 +91,5 @@ menuIcon.addEventListener('click', () => {
 closeIcon.addEventListener('click', () => {
     navLinks.classList.remove('active'); // Remove the active class to close the menu
 });
+
+
